c845ea0d88da82d56fb49cfe90138a741a03f6d9
Simona Simeonova <ssimeonova159@gmail.com>
2018-08-28 12:52:47 +0300

    Fixed home caroussel size

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -8,3 +8,11 @@ $('#close-modal-btn').onclick(closeModal);
 function closeModal(){
     $("#modalContent").parent().fadeOut();
 }
+
+
+$('#login-btn').on('click', validateLoginUrl);
+function validateLoginUrl() {
+    var currentRequestUrl = $(location).attr('href');
+    console.log(currentRequestUrl)
+}
+
